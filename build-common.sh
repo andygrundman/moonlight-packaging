90a07186b7bc8015c88ca6bae41a1484af411a4c
Andy Grundman <andy@hybridized.org>
2025-04-16 02:00:09 -0400

    Point FFmpeg-rpi at test/7.1/main, rename Docker images

--- build-common.sh
+++ build-common.sh
@@ -2,7 +2,7 @@ TAG_UNIQUE_ID=`(git ls-tree HEAD; git diff-index HEAD) | sha256sum | cut -c-16`
 TAG_NAME="${TARGET_NAME}_${TAG_UNIQUE_ID}"
 OUT_DIR="out_$TARGET_NAME"
 
-docker pull cgutman/moonlight-packaging:$TAG_NAME
+docker pull andygrundman/moonlight-packaging:$TAG_NAME
 PULL_EXIT_CODE=$?
 
 rm -rf $OUT_DIR
@@ -11,11 +11,11 @@ set -e
 mkdir $OUT_DIR
 
 if [ $PULL_EXIT_CODE -eq 0 ]; then
-  echo Using pre-built Docker image - cgutman/moonlight-packaging:$TAG_NAME
+  echo Using pre-built Docker image - andygrundman/moonlight-packaging:$TAG_NAME
 else
-  echo Pre-built image not available - building cgutman/moonlight-packaging:$TAG_NAME
-  docker build --pull -f $DOCKERFILE -t cgutman/moonlight-packaging:$TAG_NAME .
-  echo Built Docker image - cgutman/moonlight-packaging:$TAG_NAME
+  echo Pre-built image not available - building andygrundman/moonlight-packaging:$TAG_NAME
+  docker build --pull -f $DOCKERFILE -t andygrundman/moonlight-packaging:$TAG_NAME .
+  echo Built Docker image - andygrundman/moonlight-packaging:$TAG_NAME
 fi
 
-docker run --rm --mount type=bind,source="$(pwd)"/$OUT_DIR,target=/out --mount type=bind,source="$(pwd)"/debian,target=/opt/debian -e COMMIT="$1" cgutman/moonlight-packaging:$TAG_NAME
+docker run --rm --mount type=bind,source="$(pwd)"/$OUT_DIR,target=/out --mount type=bind,source="$(pwd)"/debian,target=/opt/debian -e COMMIT="$1" andygrundman/moonlight-packaging:$TAG_NAME
--- push-images.sh
+++ push-images.sh
@@ -10,39 +10,39 @@ set -e
 
 TAG_UNIQUE_ID=`git ls-tree HEAD | sha256sum | cut -c-16`
 
-docker build --pull -f Dockerfile.rpi.buster -t cgutman/moonlight-packaging:rpi-buster_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.rpi64.buster -t cgutman/moonlight-packaging:rpi64-buster_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.rpi.bookworm -t cgutman/moonlight-packaging:rpi-bookworm_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.rpi64.bookworm -t cgutman/moonlight-packaging:rpi64-bookworm_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.l4t.bionic -t cgutman/moonlight-packaging:l4t-bionic_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.rpi.buster -t andygrundman/moonlight-packaging:rpi-buster_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.rpi64.buster -t andygrundman/moonlight-packaging:rpi64-buster_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.rpi.bookworm -t andygrundman/moonlight-packaging:rpi-bookworm_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.rpi64.bookworm -t andygrundman/moonlight-packaging:rpi64-bookworm_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.l4t.bionic -t andygrundman/moonlight-packaging:l4t-bionic_$TAG_UNIQUE_ID . &
 wait
-docker build --pull -f Dockerfile.l4t.noble -t cgutman/moonlight-packaging:l4t-noble_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.aarch64.jammy -t cgutman/moonlight-packaging:aarch64-jammy_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.aarch64.noble -t cgutman/moonlight-packaging:aarch64-noble_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.armhf.jammy -t cgutman/moonlight-packaging:armhf-jammy_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.armhf.noble -t cgutman/moonlight-packaging:armhf-noble_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.l4t.noble -t andygrundman/moonlight-packaging:l4t-noble_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.aarch64.jammy -t andygrundman/moonlight-packaging:aarch64-jammy_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.aarch64.noble -t andygrundman/moonlight-packaging:aarch64-noble_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.armhf.jammy -t andygrundman/moonlight-packaging:armhf-jammy_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.armhf.noble -t andygrundman/moonlight-packaging:armhf-noble_$TAG_UNIQUE_ID . &
 wait
-docker build --pull -f Dockerfile.aarch64.bullseye -t cgutman/moonlight-packaging:aarch64-bullseye_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.aarch64.bookworm -t cgutman/moonlight-packaging:aarch64-bookworm_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.armhf.bullseye -t cgutman/moonlight-packaging:armhf-bullseye_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.armhf.bookworm -t cgutman/moonlight-packaging:armhf-bookworm_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.riscv64.sid -t cgutman/moonlight-packaging:riscv64-sid_$TAG_UNIQUE_ID . &
-docker build --pull -f Dockerfile.l4t.jammy -t cgutman/moonlight-packaging:l4t-jammy_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.aarch64.bullseye -t andygrundman/moonlight-packaging:aarch64-bullseye_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.aarch64.bookworm -t andygrundman/moonlight-packaging:aarch64-bookworm_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.armhf.bullseye -t andygrundman/moonlight-packaging:armhf-bullseye_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.armhf.bookworm -t andygrundman/moonlight-packaging:armhf-bookworm_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.riscv64.sid -t andygrundman/moonlight-packaging:riscv64-sid_$TAG_UNIQUE_ID . &
+docker build --pull -f Dockerfile.l4t.jammy -t andygrundman/moonlight-packaging:l4t-jammy_$TAG_UNIQUE_ID . &
 wait
 
-docker push cgutman/moonlight-packaging:rpi-buster_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:rpi64-buster_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:rpi-bookworm_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:rpi64-bookworm_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:l4t-bionic_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:l4t-jammy_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:l4t-noble_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:armhf-bullseye_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:armhf-bookworm_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:aarch64-bullseye_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:aarch64-bookworm_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:armhf-jammy_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:armhf-noble_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:aarch64-jammy_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:aarch64-noble_$TAG_UNIQUE_ID
-docker push cgutman/moonlight-packaging:riscv64-sid_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:rpi-buster_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:rpi64-buster_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:rpi-bookworm_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:rpi64-bookworm_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:l4t-bionic_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:l4t-jammy_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:l4t-noble_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:armhf-bullseye_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:armhf-bookworm_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:aarch64-bullseye_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:aarch64-bookworm_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:armhf-jammy_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:armhf-noble_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:aarch64-jammy_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:aarch64-noble_$TAG_UNIQUE_ID
+docker push andygrundman/moonlight-packaging:riscv64-sid_$TAG_UNIQUE_ID
